1335f682dcfc6e3f274366cad84b64eb94a64249
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-04-08 14:09:30 +0800

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,41 +20,53 @@
             <div class="mt-8">
                 
                 <div class="mt-6">
-                <form action="#" method="POST">
-                    <div>
-                    <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
-                        Alamat emel
-                    </label>
-                    <div class="mt-1 rounded-md shadow-sm">
-                        <input id="email" type="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
-                    </div>
-                    </div>
+                    <form action="#" method="POST">
+                        <div>
+                            <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
+                                Alamat emel
+                            </label>
+                            <div class="mt-1 rounded-md shadow-sm">
+                                <input id="email" type="email" name="email" value="{{ old('email') }}" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('email') border-red-500 @enderror" />
+                            </div>
 
-                    <div class="mt-6">
-                    <label for="password" class="block text-sm font-medium leading-5 text-gray-700">
-                        Kata laluan
-                    </label>
-                    <div class="mt-1 rounded-md shadow-sm">
-                        <input id="password" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" {{ old('email') }}/>
-                    </div>
-                    </div>
-
-                    <div class="mt-6 flex items-center justify-between">
-                        <div class="flex items-center text-sm">
-                            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                            Lupa kata laluan?
-                            </a>
+                            @error('email')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                            @enderror
                         </div>
-                    </div>
 
-                    <div class="mt-6">
-                        <span class="block w-full rounded-md shadow-sm">
-                            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                            Log Masuk
-                            </button>
-                        </span>
-                    </div>
-                </form>
+                        <div class="mt-6">
+                            <label for="password" class="block text-sm font-medium leading-5 text-gray-700">
+                                Kata laluan
+                            </label>
+                            <div class="mt-1 rounded-md shadow-sm">
+                                <input id="password" type="password" name="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('password') border-red-500 @enderror" />
+                            </div>
+
+                            @error('password')
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <div class="mt-6 flex items-center justify-between">
+                            <div class="flex items-center text-sm">
+                                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                                Lupa kata laluan?
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="mt-6">
+                            <span class="block w-full rounded-md shadow-sm">
+                                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                                Log Masuk
+                                </button>
+                            </span>
+                        </div>
+                    </form>
                 </div>
             </div>
             </div>
