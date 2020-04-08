6cc359b33321d6d80815ba1eef88c9124591a301
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-04-08 14:03:09 +0800

    add login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                         Kata laluan
                     </label>
                     <div class="mt-1 rounded-md shadow-sm">
-                        <input id="password" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        <input id="password" type="password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" {{ old('email') }}/>
                     </div>
                     </div>
 
