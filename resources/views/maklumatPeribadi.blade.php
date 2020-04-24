1b5d876c4f39a910809f79431ee45104a40788fc
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2020-04-24 11:11:10 +0800

    add star for taraf pendidikan

--- resources/views/maklumatPeribadi.blade.php
+++ resources/views/maklumatPeribadi.blade.php
@@ -750,7 +750,7 @@
                                 </div>
 
                                 <div class="col-span-6 sm:col-span-3">
-                                    <label for="education" class="block text-sm font-medium leading-5 text-gray-700">Taraf Pendidikan</label>
+                                    <label for="education" class="block text-sm font-medium leading-5 text-gray-700">Taraf Pendidikan <span class="text-red-700">*</span></label>
                                     <select id="education" name="bank1"
                                         class="mt-1 block form-select w-full py-2 px-3 py-0 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
                                         <option value="">Sila Pilih Taraf Pendidikan</option>
